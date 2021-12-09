5daf848308e6473c8d9c752e8d1748fa01be212b
Marie Unal <90275987+hanjisungg@users.noreply.github.com>
2021-12-09 15:23:15 +0100

    Add files via upload

---
+++ P7.py
@@ -0,0 +1,57 @@
+#exercice P7
+
+#exercice 114
+
+#figure 3
+
+def fig3(n):
+    print(n*("#"))
+    for i in range(n-2):
+        print("#", (n-2)*".", "#" )
+    print(n*("#"))
+    
+    
+    
+#exercice 115  A FAIRE
+    
+def ex115():
+    for a in range(1, 100):
+        for b in range(a, 100):
+            for c in range(b, 100):
+                if a**2 + b**2 == c**2:
+                    print(a, b, c)
+    
+def ex115prim():
+    acc = 0
+    for a in range(1, 100):
+        for b in range(a, 100):
+            for c in range(b, 100):
+                if a**2 + b**2 == c**2:
+                    acc += 1
+    return acc
+                
+    
+    
+
+#exercice 116  A FAIRE
+    
+    
+#exercice 117   IDEM
+    
+
+#exercice 118   IDEM
+    
+
+#exercice 119
+    
+
+
+    
+    
+  
+
+    
+
+    
+            
+        
