3f70e167fb089eed09fd6ca3cca8ba7d35f2f4d7
Marie Unal <90275987+hanjisungg@users.noreply.github.com>
2021-12-14 16:26:56 +0100

    Update P7.py

--- P7.py
+++ P7.py
@@ -12,38 +12,59 @@ def fig3(n):
     
     
     
-#exercice 115  A FAIRE
-    
+  #exercice 115
+
 def ex115():
-    for a in range(1, 100):
-        for b in range(a, 100):
-            for c in range(b, 100):
+    for a in range(1, 101):
+        for b in range(a, 101):
+            for c in range(b, 101):
                 if a**2 + b**2 == c**2:
                     print(a, b, c)
     
 def ex115prim():
     acc = 0
-    for a in range(1, 100):
-        for b in range(a, 100):
-            for c in range(b, 100):
+    for a in range(1, 101):
+        for b in range(a, 101):
+            for c in range(b, 101):
                 if a**2 + b**2 == c**2:
                     acc += 1
     return acc
-                
-    
-    
 
-#exercice 116  A FAIRE
-    
-    
-#exercice 117   IDEM
-    
 
-#exercice 118   IDEM
-    
+#exercice 116
 
-#exercice 119
+def ex116():
+    t = [True]*100
+    t[0] = False
+    t[1] = False
+   
+    for i in range(100):
+        if t[i] == True:
+            for j in range(i+1, 100):
+                if j%i == 0:
+                    t[j] = False
+    acc = 0         
+    for i in range(100):
+        if t[i] == True:
+            print(i)
+            acc += 1
     
+    return "nombre de nombres premiers:", acc
+
+
+#exercice 118
+
+def inclus(t1, t2):
+    acc = 0
+    for i in range(len(t1)):
+        for j in range(len(t2)):
+            if t1[i] == t2[j]:
+                acc += 1
+    if acc == len(t1):
+        return True
+    else:
+        return False
+            
 
 
     
